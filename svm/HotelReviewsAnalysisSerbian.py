4a86ca2d80f0adaa84b0063ff75ac3603c4ebeea
natasa-ivanovic <natasaivanovic98@yahoo.com>
2022-02-11 20:50:34 +0100

    WIP SVM model

--- svm/HotelReviewsAnalysisSerbian.py
+++ svm/HotelReviewsAnalysisSerbian.py
@@ -6,7 +6,8 @@ from sklearn import metrics
 from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -466,9 +467,44 @@ def preprocess(data):
 
 
 if __name__ == '__main__':
-    data = load('serbian reviews/serbian-reviews.csv')
+    data = load('../serbian reviews/serbian-reviews.csv')
     data = preprocess(data)
 
+    # without using GridSearchCV
+    x = data.review
+    y = data.sentiment
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y, test_size=0.30, random_state=101)
 
+    Tfidf_vect = TfidfVectorizer(use_idf=False, lowercase=False, sublinear_tf=True, ngram_range=(1, 2))
+    Tfidf_vect.fit(data.review)
+    x_train_tfidf = Tfidf_vect.transform(x_train)
+    x_test_tfidf = Tfidf_vect.transform(x_test)
+
+    model = SVC()
+    model.fit(x_train_tfidf, y_train)
+    predictions = model.predict(x_test_tfidf)
+    print(classification_report(y_test, predictions))
+    print("accuracy: ", accuracy_score(y_test, predictions))
+
+    # with GridSearchCV
+    tuned_parameters = {'svm__C': [1, 10, 100], 'svm__kernel': ['linear']}
+    x_train = data['review']
+    y_train = data['sentiment']
+    vectorizer = TfidfVectorizer(use_idf=False, lowercase=False, sublinear_tf=True, ngram_range=(1, 2))
+    pipeline = Pipeline([
+        ('vec', vectorizer),
+        ('svm', svm.SVC())
+    ])
+
+    grid_search = GridSearchCV(pipeline, tuned_parameters, scoring='accuracy', cv=5, error_score='raise', refit=True)
+    grid_search.fit(x_train, y_train)
+    predictions = grid_search.predict(x_train)
+    score = metrics.accuracy_score(y_train, predictions)
+    print(metrics.classification_report(y_train, predictions))
+
+    # pd.options.display.max_colwidth = 500
+    # print(data['review'].to_string())
+    # data.to_csv('result.csv', sep='|')
 
 
