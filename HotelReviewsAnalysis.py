65fc621fefa0768923b5e25bebe08f5559cec50a
natasa-ivanovic <natasaivanovic98@yahoo.com>
2022-04-11 20:13:50 +0200

    Serbian dataset + SVM grid search fine tuning

--- HotelReviewsAnalysis.py
+++ HotelReviewsAnalysis.py
@@ -10,30 +10,30 @@ if __name__ == '__main__':
     en_data = pd.read_csv('./data/english-reviews/english-reviews.csv', delimiter='|', encoding='utf-8',
                           names=['title', 'review', 'rating', 'sentiment'], skiprows=1)
     srb_data = pd.read_csv('./data/serbian-reviews/serbian-reviews.csv', delimiter='|', encoding='utf-8',
-                           names=['title', 'review', 'sentiment'], skiprows=1)
+                           names=['sentiment', 'review'], skiprows=1)
     print('>> Data successfully loaded...')
 
     print('>> English reviews preprocessing...')
-    en_data_p = ep.df_preprocess(en_data)
+    # en_data_p = ep.df_preprocess(en_data)
     print('>> Serbian reviews preprocessing...')
     srb_data_p = sp.df_preprocess(srb_data)
 
-    # print('>> Training phase for english reviews...')
-    # en_svm, en_vectorizer = SVMModel.train(en_data_p)
-    # print('>> Training phase for serbian reviews...')
-    # srb_svm, srb_vectorizer = SVMModel.train(srb_data_p)
-
     print('>> Training phase for english reviews...')
-    en_svm, en_vectorizer = RandomForestModel.train(en_data_p)
+    # en_svm, en_vectorizer = SVMModel.train(en_data_p)
     print('>> Training phase for serbian reviews...')
-    srb_svm, srb_vectorizer = RandomForestModel.train(srb_data_p)
+    srb_svm, srb_vectorizer = SVMModel.train(srb_data_p)
+
+    # print('>> Training phase for english reviews...')
+    # en_svm, en_vectorizer = RandomForestModel.train(en_data_p)
+    # print('>> Training phase for serbian reviews...')
+    # srb_svm, srb_vectorizer = RandomForestModel.train(srb_data_p)
 
     print('>> Training phase for language detection model...')
     lp_model, vectorizer = ld.train(srb_data, en_data)
 
     # test = 'Hello my dear friend I really enjoyed this hotel.'
-    # test = 'Zaista sam se dobro provela u ovom baš lepom hotelu.'
-    test = 'Ovo je užasno do bola!! Negativno iskustvo! Sobe smrde, ništa nije kao na slici!!! Odvratno je i gadi mi se.'
+    test = 'Sjajno'
+    # test = 'Ovo je užasno do bola!! Negativno iskustvo! Sobe smrde, ništa nije kao na slici!!! Odvratno je i gadi mi se.'
     # test = 'I disliked the food and the room was too small. Did not enjoy it at all. AWFUL'
     language = ld.predict(test, vectorizer, lp_model)
     if language == 0:
@@ -42,9 +42,9 @@ if __name__ == '__main__':
         data_s = ' '.join([str(elem) for elem in data])
         vectorized = srb_vectorizer.transform([data_s])
         print('Prediction: ', srb_svm.predict(vectorized)) #vraca listu predictiona umesto jednu?
-    else:
-        # english
-        data = ep.preprocess(test)
-        vectorized = en_vectorizer.transform([data])
-        print('Prediction: ', en_svm.predict(vectorized)) #vraca jednu predikciju
+    # else:
+    #     # english
+    #     data = ep.preprocess(test)
+    #     vectorized = en_vectorizer.transform([data])
+    #     print('Prediction: ', en_svm.predict(vectorized)) #vraca jednu predikciju
 
--- models/SVMModel.py
+++ models/SVMModel.py
@@ -1,7 +1,15 @@
+from sklearn import svm
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import classification_report
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
+from preprocessing import SerbianPreprocessing as sp
+from preprocessing import EnglishPreprocessing as ep
+import pandas as pd
+import joblib
+import pickle
+import os
 
 
 def tfidf_initialization(X):
@@ -14,8 +22,11 @@ def train(data):
     # without using GridSearchCV
     x = data['review']
     y = data['sentiment']
+
+    print('Negative sentiment num: ', y.values.flatten().tolist().count(-1))
+    print('Positive sentiment num: ', y.values.flatten().tolist().count(1))
     x_train, x_test, y_train, y_test = train_test_split(
-        x, y, test_size=0.30, random_state=101)
+        x, y, test_size=0.10, random_state=101)
 
     tfidf_vectorizer = tfidf_initialization(data['review'])
     x_train_tfidf = tfidf_vectorizer.transform(x_train)
@@ -29,22 +40,191 @@ def train(data):
 
     return model, tfidf_vectorizer
 
-    # # with GridSearchCV
-    # tuned_parameters = {'svm__C': [1, 10, 100], 'svm__kernel': ['sigmoid']}
-    # x_train = data['review']
-    # y_train = data['sentiment']
-    # vectorizer = TfidfVectorizer(use_idf=False, lowercase=False, sublinear_tf=True, ngram_range=(1, 2))
-    # pipeline = Pipeline([
-    #     ('vec', vectorizer),
-    #     ('svm', svm.SVC())
-    # ])
-    #
-    # grid_search = GridSearchCV(pipeline, tuned_parameters, scoring='accuracy', cv=5, error_score='raise', refit=True)
-    # grid_search.fit(x_train, y_train)
-    # predictions = grid_search.predict(x_train)
-    # score = metrics.accuracy_score(y_train, predictions)
-    # print(metrics.classification_report(y_train, predictions))
 
-    # pd.options.display.max_colwidth = 500
-    # print(data['review'].to_string())
-    # data.to_csv('result.csv', sep='|')
+def train_e_grid_search():
+    # with GridSearchCV
+    best_result = 0
+    all_results = []
+
+    for i in range(1):
+        tuned_parameters = {'svm__C': [1, 10, 100, 200], 'svm__kernel': ['linear']}
+        data = pd.read_csv('../data/english-reviews/english-reviews.csv', delimiter='|', encoding='utf-8',
+                           names=['sentiment', 'review'], skiprows=1)
+        data = ep.df_preprocess(data)
+        #
+        train, test = train_test_split(data, test_size=0.2, shuffle=True, stratify=data['sentiment'])
+        X_train = train['review'].values
+        X_test = test['review'].values
+        y_train = train['sentiment']
+        y_test = test['sentiment']
+
+        vectorizer = TfidfVectorizer(use_idf=False, lowercase=False, sublinear_tf=True, ngram_range=(1, 2))
+
+        pipeline = Pipeline([
+            ('vec', vectorizer),
+            ('svm', svm.SVC())
+        ])
+
+        grid_search = GridSearchCV(pipeline, tuned_parameters, scoring='accuracy', cv=5, error_score='raise',
+                                   refit=True, verbose=10)
+        grid_search.fit(X_train, y_train)
+        predictions = grid_search.predict(X_test)
+        score = accuracy_score(y_test, predictions)
+        all_results.append(score)
+        print('Iteration', i, 'score -', score)
+        cr = classification_report(y_test, predictions)
+        print(cr)
+
+        if score > best_result:
+            best_result = score
+            file_name = os.path.join('svm_models_english_ngram_1_2', 'svm_' + str(i) + '_' + str(round(best_result, 5)))
+            file_name_description = os.path.join('svm_models_english_ngram_1_2', 'svm_' + str(i) + '_' + str(
+                round(best_result, 5)) + '_description.txt')
+            # joblib.dump(grid_search.best_estimator_, file_name)
+            with open(file_name, 'wb') as f:
+                pickle.dump(grid_search.best_estimator_.get_params(), f)
+            # joblib.dump(grid_search.best_estimator_.get_params(), file_name)
+            f = open(file_name_description, 'w')
+            f.write('Best parameters after tuning\n')
+            f.write(str(grid_search.best_params_))
+            f.write('\n\n')
+            f.write('\nModel after parameter tuning\n')
+            f.write(str(grid_search.best_estimator_))
+            f.write('\n\n')
+            f.write('\nBest score\n')
+            f.write(str(grid_search.best_score_))
+            f.write('\n\n')
+            f.write('\nClassification report\n')
+            f.write(cr)
+            f.close()
+        print('Best score: ', best_result)
+        print('Worst score: ', min(all_results))
+
+
+def test_e_grid_search():
+    data = pd.read_csv('../data/english-reviews/english-reviews-test.csv', delimiter='|', encoding='utf-8',
+                       names=['sentiment', 'review'], skiprows=1)
+    original_reviews = data['review'].values
+
+    data = sp.df_preprocess(data)
+    # data = data.sample(frac=1)
+    # print(data)
+    X_test = data['review']
+    print(X_test.values)
+    y_test = data['sentiment']
+
+    pipeline = Pipeline([
+        ('vec', TfidfVectorizer(use_idf=False, lowercase=False, sublinear_tf=True, ngram_range=(1, 2))),
+        ('svm', svm.SVC(kernel='linear'))
+    ])
+
+    file_name = os.path.join('svm_models_english_ngram_1_2', 'svm_0_0.77128')
+    with open(file_name, 'rb') as f:
+        loaded_best_params = pickle.load(f)
+
+    pipeline.set_params(**loaded_best_params)
+    predictions = pipeline.predict(X_test)
+    print(predictions)
+    score = accuracy_score(y_test, predictions)
+    print(score)
+
+    result_df = pd.DataFrame(list(zip(predictions, y_test, original_reviews)), columns=['prediction', 'original', 'review'])
+    file_name = os.path.join('svm_result_english', 'result.csv')
+    result_df.to_csv(file_name, encoding='utf-8', sep='|', index=False)
+
+
+def train_s_grid_search():
+    # with GridSearchCV
+    best_result = 0
+    all_results = []
+
+    for i in range(1):
+        tuned_parameters = {'svm__C': [1, 10, 100, 200], 'svm__kernel': ['linear']}
+        data = pd.read_csv('../data/serbian-reviews/serbian-reviews.csv', delimiter='|', encoding='utf-8',
+                           names=['sentiment', 'review'], skiprows=1)
+        data = sp.df_preprocess(data)
+
+        train, test = train_test_split(data, test_size=0.2, shuffle=True, stratify=data['sentiment'])
+        X_train = train['review'].values
+        X_test = test['review'].values
+        y_train = train['sentiment']
+        y_test = test['sentiment']
+
+        vectorizer = TfidfVectorizer(use_idf=False, lowercase=False, sublinear_tf=True, ngram_range=(1, 2))
+
+        pipeline = Pipeline([
+            ('vec', vectorizer),
+            ('svm', svm.SVC())
+        ])
+
+        grid_search = GridSearchCV(pipeline, tuned_parameters, scoring='accuracy', cv=5, error_score='raise', refit=True, verbose=10)
+        grid_search.fit(X_train, y_train)
+        predictions = grid_search.predict(X_test)
+        score = accuracy_score(y_test, predictions)
+        all_results.append(score)
+        print('Iteration', i, 'score -', score)
+        cr = classification_report(y_test, predictions)
+        print(cr)
+
+        if score > best_result:
+            best_result = score
+            file_name = os.path.join('svm_models_serbian_ngram_1_2', 'svm_' + str(i) + '_' + str(round(best_result, 5)))
+            file_name_description = os.path.join('svm_models_serbian_ngram_1_2', 'svm_' + str(i) + '_' + str(round(best_result, 5)) + '_description.txt')
+            # joblib.dump(grid_search.best_estimator_, file_name)
+            with open(file_name, 'wb') as f:
+                pickle.dump(grid_search.best_estimator_.get_params(), f)
+            # joblib.dump(grid_search.best_estimator_.get_params(), file_name)
+            f = open(file_name_description, 'w')
+            f.write('Best parameters after tuning\n')
+            f.write(str(grid_search.best_params_))
+            f.write('\n\n')
+            f.write('\nModel after parameter tuning\n')
+            f.write(str(grid_search.best_estimator_))
+            f.write('\n\n')
+            f.write('\nBest score\n')
+            f.write(str(grid_search.best_score_))
+            f.write('\n\n')
+            f.write('\nClassification report\n')
+            f.write(cr)
+            f.close()
+        print('Best score: ', best_result)
+        print('Worst score: ', min(all_results))
+
+
+def test_s_grid_search():
+    data = pd.read_csv('../data/serbian-reviews/serbian-reviews-test.csv', delimiter='|', encoding='utf-8',
+                       names=['sentiment', 'review'], skiprows=1)
+    original_reviews = data['review'].values
+
+    data = sp.df_preprocess(data)
+    # data = data.sample(frac=1)
+    # print(data)
+    X_test = data['review']
+    print(X_test.values)
+    y_test = data['sentiment']
+
+    pipeline = Pipeline([
+        ('vec', TfidfVectorizer(use_idf=False, lowercase=False, sublinear_tf=True, ngram_range=(1, 2))),
+        ('svm', svm.SVC(kernel='linear'))
+    ])
+
+    file_name = os.path.join('svm_models_serbian_ngram_1_2', 'svm_0_0.93227')
+    with open(file_name, 'rb') as f:
+        loaded_best_params = pickle.load(f)
+
+    pipeline.set_params(**loaded_best_params)
+    predictions = pipeline.predict(X_test)
+    print(predictions)
+    score = accuracy_score(y_test, predictions)
+    print(score)
+
+    result_df = pd.DataFrame(list(zip(predictions, y_test, original_reviews)), columns=['prediction', 'original', 'review'])
+    file_name = os.path.join('svm_result_serbian', 'result.csv')
+    result_df.to_csv(file_name, encoding='utf-8', sep='|', index=False)
+
+
+if __name__ == '__main__':
+    # train_s_grid_search()
+    # test_s_grid_search()
+    # train_e_grid_search()
+    test_e_grid_search()
